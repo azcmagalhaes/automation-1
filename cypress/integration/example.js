7417dac2e4179fec33bc249ba610e3894097828a
Adriene Zilda Correa Magalhaes <vntadco@azildacorreamaga.local>
2021-02-22 11:11:28 -0300

    Template test

---
+++ cypress/integration/example.js
@@ -0,0 +1,22 @@
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+        cy.visit('/')
+        cy.wait(2000);
+    })
+
+    it('Test form', () => {
+
+    })
+
+    it('Test select', () => {
+
+    })
+
+    it('Test SEND button', () => {
+
+    })
+
+    it('Test CLEAR button', () => {
+        
+    })
+})
